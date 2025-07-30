b46efc02f383cd603406d45eb774354193f60cb6
Sravani07022006 <vootlasravani8@gmail.com>
2025-07-30 14:27:51 +0530

    adding files

---
+++ app1.py
@@ -0,0 +1,14 @@
+name = "sravani"
+number = 10.0
+ison = True
+letter = "H"
+
+# Arithmetic operations: + - * / %
+# Logical: and, or, not
+# Comparison: <, >, ==, !=, <=, >=
+# Assignment: =, +=, -=, *=
+
+if number > 10:
+    print(name)
+else:
+    print(ison)
